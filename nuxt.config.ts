b602173cc1f41fa5b577f4abb6034c5503bd7e32
Emilio Puga <ea.pugaavalos@ugto.mx>
2023-04-07 09:26:51 -0600

    Add pinia nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,14 @@
-import vuetify from "vite-plugin-vuetify";
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import vuetify from 'vite-plugin-vuetify'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  build: { transpile: ["vuetify"] },
+  build: { transpile: ['vuetify'] },
   modules: [
+    '@pinia/nuxt',
     '@nuxtjs/tailwindcss',
     async (options, nuxt) => {
-      nuxt.hooks.hook("vite:extendConfig", (config) =>
+      nuxt.hooks.hook('vite:extendConfig', (config) =>
         // @ts-ignore
         config.plugins.push(vuetify())
       )

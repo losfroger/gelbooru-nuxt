d8a1e954aec6ae613a18c3c31add33c3e7c127b3
Emilio Puga <ea.pugaavalos@ugto.mx>
2023-04-07 10:38:39 -0600

    Override rule for multi word component names

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,6 +29,7 @@ module.exports = {
 
   rules: {
     semi: ['error', 'never'],
+    'vue/multi-word-component-names': 'off',
     'quotes': [2, 'single', { 'avoidEscape': true }],
     'tailwindcss/no-custom-classname': 'off'
   },

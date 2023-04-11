57d5a570b708d7cf06e3bfcdf27ca22fe6f48b5e
Emilio P <ea.pugaavalos.99@gmail.com>
2023-04-11 00:24:50 -0600

    Fixed link text overflow, and turns into text if url isn't valid

--- components/Gelbooru/SourceLink.vue
+++ components/Gelbooru/SourceLink.vue
@@ -1,22 +1,30 @@
 <template>
-  <v-btn
-    v-bind="$attrs"
-    color="accent"
-    :disabled="typeSource.invalid"
-    :href="propsSrcLink.source"
-    target="_blank"
-    variant="tonal"
-  >
-    <img
-      v-if="typeSource.icon"
-      :src="typeSource.icon"
-      width="16"
-      class="icon tw-mr-2"
-    >
-    <span class="tw-text-white">
+  <div>
+    <span v-if="typeSource.invalid">
       {{ typeSource.text }}
     </span>
-  </v-btn>
+    <v-btn
+      v-else
+      v-bind="$attrs"
+      color="accent"
+      :disabled="typeSource.invalid"
+      :href="propsSrcLink.source"
+      target="_blank"
+      variant="tonal"
+    >
+      <div class="tw-flex tw-max-w-full tw-items-center tw-overflow-clip tw-align-middle">
+        <img
+          v-if="typeSource.icon"
+          :src="typeSource.icon"
+          width="16"
+          class="icon tw-mr-2"
+        >
+        <span class="tw-overflow-hidden tw-text-ellipsis tw-whitespace-nowrap tw-text-white">
+          {{ typeSource.text }}
+        </span>
+      </div>
+    </v-btn>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -78,6 +86,12 @@ const typeSource = computed(() => {
 </script>
 
 <style scoped>
+::v-deep .v-btn__content {
+  display: grid;/* or inline-block */
+  max-width: 100%;
+}
+
+
 .icon {
   filter: brightness(0) invert(1);
 }

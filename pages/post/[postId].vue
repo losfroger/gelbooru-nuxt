abebd8c47ef1863cbbb3276fbb1da0e395dddf99
Emilio P <ea.pugaavalos.99@gmail.com>
2023-04-11 00:27:50 -0600

    Adjusted things in post view to have double column for rtaing and source in mobile phones and ultrawide screens

--- pages/post/[postId].vue
+++ pages/post/[postId].vue
@@ -8,43 +8,54 @@
     </div>
     <div
       v-else
-      class="tw-grid tw-grid-cols-1 tw-gap-12 md:tw-grid-cols-[1fr_3fr_1fr] md:tw-gap-8"
+      class="tw-grid tw-grid-cols-1 tw-gap-12 md:tw-grid-cols-[1fr_2fr_1fr] md:tw-gap-2 lg:tw-grid-cols-[1fr_3fr_1fr] lg:tw-gap-8"
     >
       <div>
-        <div class="post-sidebar tw-flex tw-flex-col tw-items-start tw-gap-1 tw-rounded-md tw-bg-neutral-900 tw-p-4 tw-shadow-md">
-          <h5>Rating</h5>
-          <GelbooruRatingChip :rating="post?.rating" />
-          <h5 v-if="post?.source">
-            Source
-          </h5>
-          <ul v-if="post?.source">
-            <li
-              v-for="(src, i) in post.source_array"
-              :key="i"
-            >
-              <GelbooruSourceLink
-                size="small"
-                :source="src"
-              />
-            </li>
-          </ul>
-          <h5>Statistics</h5>
-          <ul>
-            <li><GelbooruUserLink :user="{creator_id: post?.creator_id ?? 0, owner: post?.owner ?? ''}" /></li>
-            <li>Id: {{ post?.id }}</li>
-            <li>Posted: {{ post?.created_at_date?.toLocaleString() }}</li>
-            <li>Size: {{ post?.width }}x{{ post?.height }}</li>
-            <li>Score: {{ post?.score.toLocaleString() }}</li>
-          </ul>
-          <v-btn
-            class="tw-mt-4"
-            color="primary"
-            append-icon="mdi-open-in-new"
-            :href="`https://gelbooru.com/index.php?page=post&s=view&id=${post?.id}`"
-            target="_blank"
+        <div class="post-sidebar tw-grid tw-grid-cols-2 tw-gap-4 tw-rounded-md tw-bg-neutral-900 tw-p-4 tw-shadow-md">
+          <div
+            :class="{
+              'md:tw-col-span-2 2xl:tw-col-span-1': true,
+              'tw-col-span-2': !post?.source
+            }"
           >
-            View on Gelbooru
-          </v-btn>
+            <h5>Rating</h5>
+            <GelbooruRatingChip :rating="post?.rating" />
+          </div>
+          <div class="md:tw-col-span-2 2xl:tw-col-span-1">
+            <h5 v-if="post?.source">
+              Source
+            </h5>
+            <ul v-if="post?.source">
+              <li
+                v-for="(src, i) in post.source_array"
+                :key="i"
+              >
+                <GelbooruSourceLink
+                  size="small"
+                  :source="src"
+                />
+              </li>
+            </ul>
+          </div>
+          <div class="tw-col-span-2">
+            <h5>Statistics</h5>
+            <div class="tw-flex tw-flex-col tw-items-start">
+              <div><GelbooruUserLink :user="{creator_id: post?.creator_id ?? 0, owner: post?.owner ?? ''}" /></div>
+              <div>Id: {{ post?.id }}</div>
+              <div>Posted: {{ post?.created_at_date?.toLocaleString() }}</div>
+              <div>Size: {{ post?.width }}x{{ post?.height }}</div>
+              <div>Score: {{ post?.score.toLocaleString() }}</div>
+            </div>
+            <v-btn
+              color="primary"
+              class="tw-mt-4"
+              append-icon="mdi-open-in-new"
+              :href="`https://gelbooru.com/index.php?page=post&s=view&id=${post?.id}`"
+              target="_blank"
+            >
+              View on Gelbooru
+            </v-btn>
+          </div>
         </div>
       </div>
       <div class="tw-overflow-clip">
@@ -80,27 +91,33 @@
             </v-img>
           </a>
         </div>
-        <v-img
+        <div
           v-else
-          max-height="88vh"
-          width="100%"
-          :lazy-src="post?.preview_url"
-          :src="`/api/image/${post?.id}`"
-          :class="{
-            'tw-drop-shadow-xl tw-transition-all': true,
-            'tw-blur-2xl hover:tw-blur-none': settingStore.settings.hideNsfwImages && isNsfw
-          }"
-        />
+          class="tw-flex tw-flex-col tw-items-center tw-gap-2"
+        >
+          <v-img
+            max-height="88vh"
+            width="100%"
+            :lazy-src="post?.preview_url"
+            :src="`/api/image/${post?.id}`"
+            :class="{
+              'tw-drop-shadow-xl tw-transition-all': true,
+              'tw-blur-2xl hover:tw-blur-none': settingStore.settings.hideNsfwImages && isNsfw
+            }"
+          />
+        </div>
       </div>
       <div>
-        <div class="post-sidebar tw-flex tw-flex-col tw-items-start tw-gap-1 tw-rounded-md tw-bg-neutral-900 tw-p-4 tw-shadow-md">
-          <h5>Tags</h5>
-          <div class="tw-flex tw-flex-row tw-flex-wrap tw-gap-3 md:tw-gap-1">
-            <GelbooruTagChip
-              v-for="tag in post?.tags_array"
-              :key="tag"
-              :tag="tag"
-            />
+        <div class="post-sidebar tw-flex tw-flex-col tw-items-start tw-gap-4 tw-rounded-md tw-bg-neutral-900 tw-p-4 tw-shadow-md">
+          <div>
+            <h5>Tags</h5>
+            <div class="tw-flex tw-flex-row tw-flex-wrap tw-gap-3 md:tw-gap-1">
+              <GelbooruTagChip
+                v-for="tag in post?.tags_array"
+                :key="tag"
+                :tag="tag"
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -134,11 +151,11 @@ const isNsfw = computed(() => !sfwRatings.includes(post.value?.rating ?? ''))
 li {
   @apply tw-list-none;
 }
-.post-sidebar > h5 {
-  @apply tw-w-full tw-mt-4 tw-mb-4;
+.post-sidebar > div > h5 {
+  @apply tw-w-full tw-mb-4;
 }
 
-.post-sidebar > h5::after {
+.post-sidebar > div > h5::after {
   @apply tw-bg-neutral-800 tw-rounded-full;
   content: "";
   float: left;

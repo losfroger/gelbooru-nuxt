e2d0e206c199c52240830d6d44b132191351e1cf
Emilio P <ea.pugaavalos.99@gmail.com>
2023-04-16 01:24:21 -0600

    Added notifications to appstore to show notifiactions to user

---
+++ components/App/NotificationsAlerts.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="tw-flex tw-flex-col tw-gap-2">
+    <v-slide-x-reverse-transition
+      group
+    >
+      <AppNotificationAlert
+        v-for="notification in appStore.notification"
+        :key="notification.uuid"
+        :notification="notification"
+      />
+    </v-slide-x-reverse-transition>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useAppStore } from '~/stores/appStore'
+
+const appStore = useAppStore()
+
+</script>
+
+<style scoped>
+
+</style>
--- stores/appStore.ts
+++ stores/appStore.ts
@@ -1,9 +1,45 @@
+export interface NotificationInterface {
+  uuid: string,
+  title?: string,
+  text?: string,
+  icon?: string,
+  timeout: number,
+  color: string
+}
+
 export const useAppStore = defineStore('appStore', () => {
   const toggleMenu = ref(true)
   const loading = ref(false)
 
+  const notification = ref<NotificationInterface[]>([])
+
+  function addNotification(options: {
+    title?: string,
+    text?: string,
+    icon?: string,
+    timeout?: number,
+    color?: string
+  }) {
+    notification.value.push({
+      uuid: Date.now().toString(),
+      title: options.title,
+      text: options.text,
+      icon: options.icon,
+      timeout: options.timeout ?? 5000,
+      color: options.color ?? 'success'
+    })
+  }
+
+  function removeNotification(uuid: string) {
+    notification.value = notification.value.filter((notif) => notif.uuid !== uuid)
+  }
+
+
   return {
     toggleMenu,
     loading,
+    notification,
+    addNotification,
+    removeNotification,
   }
 })

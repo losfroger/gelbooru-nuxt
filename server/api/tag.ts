ae451dcab35424571202968485db71476c1ff039
Emilio P <ea.pugaavalos.99@gmail.com>
2023-04-17 23:45:42 -0600

    Lots of changes to tag endpoint. Now it can detect when a query is negative, fuzzy or if it has a wildcard.  It also reacts better when there's no tag results

--- server/api/tag.ts
+++ server/api/tag.ts
@@ -1,9 +1,40 @@
 import axios_gelbooru from '~/server/axiosGelbooru'
-import { GelbooruTag, GelbooruTagRes } from '~/types/gelbooru'
+import { GelbooruTag, GelbooruTagRes, GelbooruTagReq, NegativeQueryRegex, FuzzyQueryRegex } from '~/types/gelbooru'
 
-export default defineEventHandler(async (event): Promise<GelbooruTagRes> => {
+export default defineEventHandler(async (event): Promise<GelbooruTagRes | undefined> => {
   try {
-    const query = getQuery(event)
+    const tagQuery: GelbooruTagReq = getQuery(event)
+
+    let negativeQuery = false
+    let fuzzyQuery = false
+    if (tagQuery.name_pattern) {
+      // Negative query
+      if (NegativeQueryRegex.test(tagQuery.name_pattern)) {
+        negativeQuery = true
+        console.log('Negative query', tagQuery.name_pattern)
+
+        tagQuery.name_pattern = tagQuery.name_pattern.replaceAll('-', '')
+      }
+
+      // Fuzy query
+      else if (FuzzyQueryRegex.test(tagQuery.name_pattern)) {
+        fuzzyQuery = true
+        console.log('Fuzzy query', tagQuery.name_pattern)
+
+        tagQuery.name_pattern = tagQuery.name_pattern.replaceAll('~', '')
+      }
+
+      // If has wildcard
+      if (tagQuery.name_pattern.includes('*')) {
+        tagQuery.name_pattern = tagQuery.name_pattern.replaceAll('*', '%')
+      }
+
+      // Else, use %name_pattern%
+      else {
+        tagQuery.name_pattern = `%${tagQuery.name_pattern}%`
+      }
+    }
+
     const resGel = await axios_gelbooru.get<GelbooruTagRes>('', {
       params: {
         page: 'dapi',
@@ -13,11 +44,20 @@ export default defineEventHandler(async (event): Promise<GelbooruTagRes> => {
         json: 1,
         api_key: event.node.req.headers.api_key,
         user_id: event.node.req.headers.user_id,
-        name_pattern: query.name_pattern,
-        name: query.name,
-        orderby: query.orderby ?? 'count'
+        name_pattern: tagQuery.name_pattern,
+        name: tagQuery.name,
+        orderby: tagQuery.orderby ?? 'count'
       }
     })
+
+    console.debug(tagQuery.name_pattern, resGel.data['@attributes'])
+
+    if (!resGel.data.tag) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'No tags were found with that criteria'
+      })
+    }
 
     resGel.data.tag.forEach((tag: GelbooruTag) => {
       switch (tag.type) {
@@ -43,6 +83,13 @@ export default defineEventHandler(async (event): Promise<GelbooruTagRes> => {
           tag.type_string = 'not identified'
           break
       }
+
+      if (fuzzyQuery) {
+        tag.name = `${tag.name}~`
+      } else if (negativeQuery) {
+        tag.name = `-${tag.name}`
+      }
+
     })
 
     return resGel.data

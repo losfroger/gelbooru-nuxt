e1007a27492d93d7e441fd715ab9364512fb7c41
Emilio P <ea.pugaavalos.99@gmail.com>
2023-04-18 19:22:31 -0600

    Lots of changes to favorites view to get it working with search params

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -1,8 +1,35 @@
 <template>
   <div class="view-container tw-grid tw-grid-cols-1">
-    {{ error }}
-    <div v-if="data">
+    <div>
+      <div>
+        <GelbooruSearchBar v-model="tags" />
+        <div
+          v-if="error"
+          class="text-caption tw-mr-auto tw-mt-1"
+        >
+          0 posts
+        </div>
+        <div
+          v-else-if="data"
+          class="text-caption tw-mr-auto tw-mt-1"
+        >
+          {{ data['@attributes'].count.toLocaleString() }} posts
+        </div>
+      </div>
       <div
+        v-if="error"
+        class="tw-flex tw-flex-col tw-items-center tw-gap-4 tw-py-16"
+      >
+        {{ error.statusMessage }}
+        <v-btn
+          prepend-icon="mdi-arrow-left"
+          @click="router.back()"
+        >
+          Back
+        </v-btn>
+      </div>
+      <div
+        v-else-if="data"
         class="tw-mt-8 tw-grid tw-grid-cols-1 tw-gap-4 sm:tw-grid-cols-2 md:tw-grid-cols-4 lg:tw-grid-cols-5 2xl:tw-grid-cols-10"
       >
         <v-slide-y-reverse-transition group>
@@ -13,7 +40,7 @@
           />
         </v-slide-y-reverse-transition>
       </div>
-      <div>
+      <div v-if="data">
         <v-pagination
           v-model="currentPage"
           class="tw-col-span-full tw-mt-8 md:tw-mx-8"
@@ -51,6 +78,8 @@ useHead({
 
 const pid = computed(() => currentPage.value - 1)
 const currentPage = ref(parseInt(route.query.page?.toString() ?? '1'))
+console.log('Starting tags', route.query.tags?.toString())
+const tags = ref(route.query.tags?.toString() ?? 'sort:score')
 
 // Max page count is 400
 const pageCount = computed(() => Math.min(
@@ -67,24 +96,44 @@ watch(() => route.query.page, (newVal) => {
   currentPage.value = parseInt(newVal?.toString() ?? '1')
 })
 
-// Use refresh to fetch data with new page
-watch(currentPage, (newValue, oldValue) => {
-  if (newValue == oldValue) {
-    return
-  }
-
-  console.log('Pushing new page!')
-  router.push(`/favorites?page=${newValue}`)
-
-  appStore.loading = true
-
-  refresh()
+watch(() => route.query.tags, (newVal) => {
+  console.log('Tags', newVal?.toString())
+  tags.value = newVal?.toString() ?? ''
 })
 
-const { data, error, refresh } = await useFetch<GelbooruPostRes>('/api/post/favorites', {
+// Reset page when tags change
+watch(tags, (newValue, oldValue) => {
+  if (newValue !== oldValue) {
+    currentPage.value = 1
+  }
+})
+
+// Push on router when tags or current page change
+watch(() => [tags, currentPage], () => {
+  const query: string[] = []
+
+  if (currentPage.value) {
+    query.push(`page=${currentPage.value}`)
+  }
+
+  if (tags.value) {
+    query.push(`tags=${tags.value}`)
+  }
+
+  router.push(`/favorites?${query.join('&')}`)
+}, {deep:true})
+
+const firstLoad = ref(false)
+const { data, error } = await useFetch<GelbooruPostRes>('/api/post/favorites', {
   query: {
     pid,
+    tags,
     limit: 50,
+  },
+  onRequest: () => {
+    if (process.client && firstLoad.value) {
+      appStore.loading = true
+    }
   },
   onResponse: () => {
     window.scroll(0, 0)
@@ -94,6 +143,7 @@ const { data, error, refresh } = await useFetch<GelbooruPostRes>('/api/post/favo
   }
 })
 
+firstLoad.value = true
 
 </script>
 

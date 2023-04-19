2b936f718a9998197581b48a356f95c59efd0966
Emilio P <ea.pugaavalos.99@gmail.com>
2023-04-18 18:59:49 -0600

    Remove try catch

--- server/api/tag.ts
+++ server/api/tag.ts
@@ -1,101 +1,95 @@
 import axios_gelbooru from '~/server/axiosGelbooru'
 import { GelbooruTag, GelbooruTagRes, GelbooruTagReq, NegativeQueryRegex, FuzzyQueryRegex } from '~/types/gelbooru'
 
-export default defineEventHandler(async (event): Promise<GelbooruTagRes | undefined> => {
-  try {
-    const tagQuery: GelbooruTagReq = getQuery(event)
+export default defineEventHandler(async (event) => {
+  const tagQuery: GelbooruTagReq = getQuery(event)
 
-    let negativeQuery = false
-    let fuzzyQuery = false
-    if (tagQuery.name_pattern) {
-      // Negative query
-      if (NegativeQueryRegex.test(tagQuery.name_pattern)) {
-        negativeQuery = true
-        console.log('Negative query', tagQuery.name_pattern)
+  let negativeQuery = false
+  let fuzzyQuery = false
+  if (tagQuery.name_pattern) {
+    // Negative query
+    if (NegativeQueryRegex.test(tagQuery.name_pattern)) {
+      negativeQuery = true
+      console.log('Negative query', tagQuery.name_pattern)
 
-        tagQuery.name_pattern = tagQuery.name_pattern.replaceAll('-', '')
-      }
-
-      // Fuzy query
-      else if (FuzzyQueryRegex.test(tagQuery.name_pattern)) {
-        fuzzyQuery = true
-        console.log('Fuzzy query', tagQuery.name_pattern)
-
-        tagQuery.name_pattern = tagQuery.name_pattern.replaceAll('~', '')
-      }
-
-      // If has wildcard
-      if (tagQuery.name_pattern.includes('*')) {
-        tagQuery.name_pattern = tagQuery.name_pattern.replaceAll('*', '%')
-      }
-
-      // Else, use %name_pattern%
-      else {
-        tagQuery.name_pattern = `%${tagQuery.name_pattern}%`
-      }
+      tagQuery.name_pattern = tagQuery.name_pattern.replaceAll('-', '')
     }
 
-    const resGel = await axios_gelbooru.get<GelbooruTagRes>('', {
-      params: {
-        page: 'dapi',
-        q: 'index',
-        s: 'tag',
-        limit: 15,
-        json: 1,
-        api_key: event.node.req.headers.api_key,
-        user_id: event.node.req.headers.user_id,
-        name_pattern: tagQuery.name_pattern,
-        name: tagQuery.name,
-        orderby: tagQuery.orderby ?? 'count'
-      }
-    })
+    // Fuzy query
+    else if (FuzzyQueryRegex.test(tagQuery.name_pattern)) {
+      fuzzyQuery = true
+      console.log('Fuzzy query', tagQuery.name_pattern)
 
-    console.debug(tagQuery.name_pattern, resGel.data['@attributes'])
-
-    if (!resGel.data.tag) {
-      throw createError({
-        statusCode: 400,
-        statusMessage: 'No tags were found with that criteria'
-      })
+      tagQuery.name_pattern = tagQuery.name_pattern.replaceAll('~', '')
     }
 
-    resGel.data.tag.forEach((tag: GelbooruTag) => {
-      switch (tag.type) {
-        case 0:
-          tag.type_string = 'general'
-          break
-        case 1:
-          tag.type_string = 'artist'
-          break
-        case 3:
-          tag.type_string = 'copyright'
-          break
-        case 4:
-          tag.type_string = 'character'
-          break
-        case 5:
-          tag.type_string = 'metadata'
-          break
-        case 6:
-          tag.type_string = 'deprecated'
-          break
-        default:
-          tag.type_string = 'not identified'
-          break
-      }
+    // If has wildcard
+    if (tagQuery.name_pattern.includes('*')) {
+      tagQuery.name_pattern = tagQuery.name_pattern.replaceAll('*', '%')
+    }
 
-      if (fuzzyQuery) {
-        tag.name = `${tag.name}~`
-      } else if (negativeQuery) {
-        tag.name = `-${tag.name}`
-      }
-
-    })
-
-    return resGel.data
-  } catch (error) {
-    console.log(error)
-    throw error
+    // Else, use %name_pattern%
+    else {
+      tagQuery.name_pattern = `%${tagQuery.name_pattern}%`
+    }
   }
 
+  const resGel = await axios_gelbooru.get<GelbooruTagRes>('', {
+    params: {
+      page: 'dapi',
+      q: 'index',
+      s: 'tag',
+      limit: 15,
+      json: 1,
+      api_key: event.node.req.headers.api_key,
+      user_id: event.node.req.headers.user_id,
+      name_pattern: tagQuery.name_pattern,
+      name: tagQuery.name,
+      orderby: tagQuery.orderby ?? 'count'
+    }
+  })
+
+  console.debug(tagQuery.name_pattern, resGel.data['@attributes'])
+
+  if (!resGel.data.tag) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'No tags were found with that criteria'
+    })
+  }
+
+  resGel.data.tag.forEach((tag: GelbooruTag) => {
+    switch (tag.type) {
+      case 0:
+        tag.type_string = 'general'
+        break
+      case 1:
+        tag.type_string = 'artist'
+        break
+      case 3:
+        tag.type_string = 'copyright'
+        break
+      case 4:
+        tag.type_string = 'character'
+        break
+      case 5:
+        tag.type_string = 'metadata'
+        break
+      case 6:
+        tag.type_string = 'deprecated'
+        break
+      default:
+        tag.type_string = 'not identified'
+        break
+    }
+
+    if (fuzzyQuery) {
+      tag.name = `${tag.name}~`
+    } else if (negativeQuery) {
+      tag.name = `-${tag.name}`
+    }
+
+  })
+
+  return resGel.data
 })

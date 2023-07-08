d49a29e64f6983ce132921c8823c1f1e27ede851
Emilio P <ea.pugaavalos.99@gmail.com>
2023-07-07 20:04:48 -0600

    Fixed problem with number of posts per page input

--- pages/settings.vue
+++ pages/settings.vue
@@ -9,41 +9,47 @@
         My settings
       </v-card-title>
       <v-divider class="tw-my-4" />
-      <v-card-text class="tw-flex tw-flex-col tw-gap-10 tw-pb-8">
-        <v-switch
-          v-model="auxSettings.hideNsfwImages"
-          inset
-          label="Blur NSFW images"
-          color="primary"
-          hint="Will blur NSFW images until hovered over."
-          persistent-hint
-        />
-        <GelbooruTagSelector
-          v-model="auxSettings.filteredTags"
-          label="Tag Blacklist"
-          prepend-inner-icon="mdi-cancel"
-          :clearable="false"
-          hide-default-items
-          hint="Any post containing a blacklisted tag will be ignored."
-          persistent-hint
-        />
-        <v-text-field
-          v-model="auxSettings.numberPostsPerPage"
-          label="Number of posts per page"
-          color="primary"
-          type="number"
-          inputmode="numeric"
-          pattern="[0-9]*"
-          required
-          min="1"
-          step="1"
-          @input="(e: Event) => {
-            // Only allow positive integer numbers
-            let aux = auxSettings.numberPostsPerPage.toString().replace(/[^0-9.]/g, '')
-            aux = aux.replace(/(\..*)\./g, '$1')
-            auxSettings.numberPostsPerPage = isFinite(parseInt(aux)) ? Math.max(1, parseInt(aux)) : 1
-          }"
-        />
+      <v-card-text>
+        <v-form
+          v-model="validSettingsForm"
+          class="tw-flex tw-flex-col tw-gap-8 tw-pb-8"
+        >
+          <v-switch
+            v-model="auxSettings.hideNsfwImages"
+            inset
+            label="Blur NSFW images"
+            color="primary"
+            hint="Will blur NSFW images until hovered over."
+            persistent-hint
+          />
+          <GelbooruTagSelector
+            v-model="auxSettings.filteredTags"
+            label="Tag Blacklist"
+            prepend-inner-icon="mdi-cancel"
+            :clearable="false"
+            hide-default-items
+            hint="Any post containing a blacklisted tag will be ignored."
+            persistent-hint
+          />
+          <v-text-field
+            v-model="auxNumberOfPostsPerPage"
+            label="Number of posts per page"
+            color="primary"
+            type="number"
+            inputmode="numeric"
+            pattern="[0-9]*"
+            required
+            min="1"
+            step="1"
+            :rules="[v => parseInt(v) > 0 || 'Please enter a valid number']"
+            @input="(e: Event) => {
+              // Only allow positive integer numbers
+              let aux = auxNumberOfPostsPerPage.toString().replace(/[^0-9.]/g, '')
+              aux = aux.replace(/(\..*)\./g, '$1')
+              auxNumberOfPostsPerPage = isFinite(parseInt(aux)) ? Math.max(1, parseInt(aux)).toString() : ''
+            }"
+          />
+        </v-form>
       </v-card-text>
       <v-card-actions class="tw-flex tw-flex-col tw-items-start tw-gap-2 md:tw-flex-row md:tw-items-center">
         <v-fade-transition>
@@ -83,18 +89,25 @@ useHead({
 
 const settingsStore = useSettingsStore()
 const auxSettings = ref<UserSettings>({...settingsStore.settings})
+const auxNumberOfPostsPerPage = ref(settingsStore.settings.numberPostsPerPage.toString())
+
 const changes = ref(false)
 
-watch(auxSettings, () => {
-  changes.value = true
-}, {deep: true})
+watch(auxSettings, () => changes.value = true, {deep: true})
+watch(auxNumberOfPostsPerPage, () => changes.value = true)
+
+const validSettingsForm = ref(false)
 
 function saveSettings() {
+  if (!validSettingsForm.value) {
+    return
+  }
+
   console.log('Saved settings!')
 
   settingsStore.settings.filteredTags = auxSettings.value.filteredTags
   settingsStore.settings.hideNsfwImages = auxSettings.value.hideNsfwImages
-  settingsStore.settings.numberPostsPerPage = auxSettings.value.numberPostsPerPage
+  settingsStore.settings.numberPostsPerPage = parseInt(auxNumberOfPostsPerPage.value)
 
   changes.value = false
 }

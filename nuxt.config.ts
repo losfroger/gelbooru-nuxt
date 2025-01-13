0fb36bb4d6ba56283a965b08910f2930edb2c361
Emilio P <ea.pugaavalos.99@gmail.com>
2025-01-12 21:32:04 -0600

    Configure tailwind and quasar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxtjs/tailwindcss',
     'nuxt-quasar-ui',
   ],
+  runtimeConfig: {
+    public: {
+      VERSION: 'rewrite 0.0.1',
+    },
+  },
+  tailwindcss: {
+    cssPath: '@/assets/css/tailwind.css',
+  },
+  quasar: {
+    iconSet: 'mdi-v7',
+    quietSassWarnings: true,
+    config: {
+      ripple: true,
+      dark: 'auto',
+    },
+    extras: {
+      animations: 'all',
+    },
+    plugins: [
+      'Notify',
+      'Dark',
+    ],
+  },
 })

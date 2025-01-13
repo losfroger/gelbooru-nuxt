fe799e422e5b5ec8d0738965ec1f7a45075e10f5
Emilio P <ea.pugaavalos.99@gmail.com>
2025-01-12 23:30:59 -0600

    Barebones post page

---
+++ pages/post/[postId].vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="tw-grid tw-grid-cols-[1fr_3fr_1fr] tw-gap-4">
+      <div>
+        <QCard flat>
+          <QCardSection class="tw-flex tw-flex-col tw-gap-2">
+            <QBtn
+              label="View on Gelbooru"
+              icon="mdi-open-in-new"
+              color="primary"
+              outline
+              :href="`https://gelbooru.com/index.php?page=post&s=view&id=${post?.id}`"
+              target="_blank"
+            />
+            <QBtn
+              label="View full image"
+              icon="mdi-image-outline"
+              color="secondary"
+              outline
+              :href="post?.file_url ? `/api/image/full/${post?.id}` : `/api/image/${post?.id}`"
+              target="_blank"
+            />
+          </QCardSection>
+        </QCard>
+      </div>
+      <QImg
+        :placeholder-src="post?.preview_url"
+        :src="`/api/image/${post?.id}`"
+        fit="contain"
+        class="tw-max-h-[80vh]"
+      />
+      <div>
+        <QCard flat>
+          <QCardSection class="tw-flex tw-flex-row tw-flex-wrap tw-gap-2">
+            <QChip
+              v-for="tag in post?.tags_array"
+              :key="tag"
+              :label="tag"
+              outline
+              color="primary"
+              class="tw-m-0"
+            />
+          </QCardSection>
+        </QCard>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { GelbooruPostWithTags } from '~/types/gelbooru'
+
+
+const route = useRoute()
+const postId = route.params.postId
+
+const { data: post, status  } = await useFetch<GelbooruPostWithTags>(`/api/post/${postId}`, {
+  parseResponse: (res) => {
+    const aux: GelbooruPostWithTags = JSON.parse(res)
+    if (aux.created_at_date) {
+      aux.created_at_date = new Date(aux.created_at_date)
+    }
+
+    return aux
+  },
+})
+
+</script>
+
+<style scoped>
+
+</style>

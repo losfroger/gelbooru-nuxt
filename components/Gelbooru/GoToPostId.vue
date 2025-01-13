7932414a374ec77e1e36c83eac21ed72ffb3d1a3
Emilio P <ea.pugaavalos.99@gmail.com>
2025-01-12 23:37:30 -0600

    Add go to post id dialog

---
+++ components/Gelbooru/GoToPostId.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <QBtn
+      label="Go to post ID"
+      color="secondary"
+    >
+      <QMenu>
+        <QCard>
+          <QCardSection>
+            <QInput
+              v-model="postId"
+              type="number"
+              label="ID"
+            />
+            <QBtn label="Go" @click="goToPostId" />
+          </QCardSection>
+        </QCard>
+      </QMenu>
+    </QBtn>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const router = useRouter()
+const postId = ref('')
+
+function goToPostId() {
+  if (!postId.value) {
+    return
+  }
+
+  router.push(`/post/${postId.value}`)
+}
+
+</script>
+
+<style scoped>
+
+</style>

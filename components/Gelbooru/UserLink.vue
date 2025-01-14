28deec23ac3b4c3b5ffe21b862728298e11b5270
Emilio P <ea.pugaavalos.99@gmail.com>
2025-01-13 21:15:26 -0600

    User link component

---
+++ components/Gelbooru/UserLink.vue
@@ -0,0 +1,29 @@
+<template>
+  <a
+    class="tw-flex tw-flex-row tw-items-center tw-text-white tw-no-underline tw-transition-colors visited:tw-text-white hover:tw-text-accent"
+    :href="`https://gelbooru.com/index.php?page=account&s=profile&id=${props.user.creator_id}`"
+    target="_blank"
+  >
+    <QIcon
+      name="mdi-account"
+      size="xs"
+      class="tw-mr-1"
+    />
+    {{ props.user.owner }}
+  </a>
+</template>
+
+<script setup lang="ts">
+import type { GelbooruUser } from '~/types/gelbooru'
+
+interface GelbooruUserLinkProps {
+  user: GelbooruUser
+}
+
+const props = defineProps<GelbooruUserLinkProps>()
+
+</script>
+
+<style scoped>
+
+</style>

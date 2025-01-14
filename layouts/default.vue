506ecf3feb1bec51f44ba743bdd98daa87c7feef
Emilio P <ea.pugaavalos.99@gmail.com>
2025-01-13 19:23:34 -0600

    Add menu button and icon on mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,12 +4,31 @@
     view="lHh lpR fff"
   >
     <QHeader reveal class="tw-bg-transparent tw-shadow-lg tw-shadow-black/25">
-      <QToolbar class="tw-flex tw-flex-row tw-gap-4 tw-bg-neutral-700 tw-px-6 tw-py-3 dark:tw-bg-neutral-800">
-        <h1 class="text-h5">
-          Gelbooru
-        </h1>
+      <QToolbar class="tw-bg-neutral-700 tw-px-6 tw-py-3 dark:tw-bg-neutral-800">
+        <div v-auto-animate class="tw-flex tw-flex-1 tw-flex-row tw-items-center tw-gap-2">
+          <QBtn
+            v-if="$q.screen.lt.md"
+            icon="mdi-menu"
+            dense
+            round
+            flat
+            class="tw-my-auto"
+            @click="leftDrawerOpen = true"
+          />
+          <NuxtLink
+            v-if="$q.screen.lt.md"
+            to="/"
+            class="tw-my-auto"
+          >
+            <img src="/gelbooru-logo.svg" class="tw-my-auto tw-h-auto tw-w-7">
+          </NuxtLink>
+          <QToolbarTitle>
+            Gelbooru
+          </QToolbarTitle>
+        </div>
       </QToolbar>
     </QHeader>
+
     <QDrawer
       v-model="leftDrawerOpen"
       side="left"
@@ -19,7 +38,7 @@
       @mouseenter="miniState = false"
       @mouseleave="miniState = true"
     >
-      <div class="tw-py-1.5">
+      <div v-if="$q.screen.gt.sm" class="tw-py-1.5">
         <NuxtLink
           to="/"
           class="tw-my-auto tw-flex tw-flex-col tw-justify-center tw-rounded-sm tw-align-middle tw-transition-colors hover:tw-bg-neutral-700"
@@ -59,16 +78,20 @@
         </QItem>
       </QList>
     </QDrawer>
+
     <QPageContainer class="page-container tw-flex tw-max-w-[100vw] tw-flex-col tw-items-center tw-overflow-hidden tw-bg-[#f2f2f2] dark:tw-bg-neutral-900">
       <QPage padding class="tw-min-h-[90vh] tw-w-full tw-max-w-[1400px] tw-p-4 tw-pb-14 md:tw-p-6">
         <NuxtPage />
       </QPage>
     </QPageContainer>
+
     <PageFooter />
   </QLayout>
 </template>
 
 <script setup lang="ts">
+import { QToolbarTitle } from 'quasar'
+
 
 const miniState = ref(true)
 const leftDrawerOpen = ref(true)

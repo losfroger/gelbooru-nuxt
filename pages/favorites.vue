5abb36f1913fd603cea6fa59d51d15704df78419
Emilio P <ea.pugaavalos.99@gmail.com>
2025-01-13 21:15:47 -0600

    Start favorites view

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -1,10 +1,31 @@
 <template>
   <div>
-    Favorites!
+    <div v-if="favorites?.post && status =='success'" class="tw-grid tw-grid-cols-4 tw-gap-4">
+      <GelbooruPostCard
+        v-for="post in favorites.post"
+        :key="post.id"
+        :post="post"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { GelbooruPostRes } from '~/types/gelbooru'
+
+
+const route = useRoute()
+
+const currentPage = ref(parseInt(route.query.page?.toString() ?? '1'))
+const pid = computed(() => currentPage.value - 1)
+const tags = ref(route.query.tags?.toString() ?? 'sort:score')
+
+const { data: favorites, status } = await useFetch<GelbooruPostRes>('/api/post/favorites', {
+  query: {
+    pid,
+    tags,
+  },
+})
 
 </script>
 

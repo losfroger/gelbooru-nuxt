43e26bb6564f2caf589840e4004270a6d18a96b6
Emilio P <ea.pugaavalos.99@gmail.com>
2025-01-16 23:20:58 -0600

    Search bar components

---
+++ components/Gelbooru/SearchBarChipTag.vue
@@ -0,0 +1,55 @@
+<template>
+  <QChip
+    v-bind="props"
+    :color="color"
+  />
+</template>
+
+<script setup lang="ts">
+/**
+ * Tag for use inside the search bar, automatically changes color based on the label to
+ * indicate different types of searches
+ */
+import type { NamedColor, QChipProps } from 'quasar'
+import { FuzzyQueryRegex, NegativeQueryRegex } from '~/types/gelbooru'
+
+
+interface GelbooruSearchBarChipTagProps extends Omit<QChipProps, 'label' | 'modelValue' | 'dark' | 'color' | 'onUpdate:modelValue'> {
+  label: string,
+  negativeTagColor?: NamedColor,
+  fuzzyTagColor?: NamedColor,
+  wildCardTagColor?: NamedColor,
+  normalTagColor?: NamedColor,
+}
+
+const props = withDefaults(defineProps<GelbooruSearchBarChipTagProps>(), {
+  negativeTagColor: 'red',
+  fuzzyTagColor: 'indigo',
+  wildCardTagColor: 'orange',
+  normalTagColor: 'grey-10',
+})
+
+const color = computed(() => {
+  // Is negative
+  if (NegativeQueryRegex.test(props.label)) {
+    return props.negativeTagColor
+  }
+  // Is fuzzy
+  else if (FuzzyQueryRegex.test(props.label)) {
+    return props.fuzzyTagColor
+  }
+  //Wild card
+  else if (props.label.includes('*')) {
+    return props.wildCardTagColor
+  }
+  // Normal color
+  else {
+    return props.normalTagColor
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>

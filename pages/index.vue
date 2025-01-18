f1292f40c02c52fe3e918dc2dd630e6cfeee4443
Emilio P <ea.pugaavalos.99@gmail.com>
2025-01-17 18:10:28 -0600

    Add links under search bar, add automatic refresh to the count, and make the circular progress like the global app one

--- pages/index.vue
+++ pages/index.vue
@@ -9,18 +9,20 @@
         Gelbooru
       </h1>
     </a>
-    <div v-auto-animate class="counter-hero-banner tw-relative tw-flex tw-min-h-8 tw-flex-row">
+    <div v-auto-animate class="counter-hero-banner tw-relative tw-flex tw-min-h-14 tw-flex-row">
       <PageCuteCounter
         v-for="(number, idx) in postCount?.toString()"
         :key="idx"
         :number="number"
       />
-      <div v-if="status == 'pending'" class="absolute-center tw-rounded-full tw-bg-neutral-800/50 tw-p-2 tw-backdrop-blur-sm">
+      <div v-if="status == 'pending'" class="absolute-center tw-rounded-full tw-bg-neutral-950/70 tw-p-2 tw-backdrop-blur-sm">
         <QCircularProgress
           color="accent"
           indeterminate
+          track-color="dark"
           size="lg"
           rounded
+          :thickness="0.3"
         />
       </div>
     </div>
@@ -34,7 +36,7 @@
           filled
           clearable
           hide-bottom-space
-          class="tw-w-full tw-max-w-4xl"
+          class="tw-w-full tw-max-w-3xl"
         />
         <QBtn
           type="submit"
@@ -44,6 +46,32 @@
           class="tw-my-auto"
         />
       </QForm>
+      <div class="links-hero-wrapper tw-flex tw-flex-row tw-gap-4">
+        <a
+          href="https://gelbooru.com/index.php?page=comment&s=list"
+          target="_blank"
+        >
+          Comments
+        </a>
+        <a
+          href="https://gelbooru.com/index.php?page=forum&s=list"
+          target="_blank"
+        >
+          Forum
+        </a>
+        <a
+          href="https://gelbooru.com/index.php?page=wiki&s=list"
+          target="_blank"
+        >
+          Wiki
+        </a>
+        <a
+          href="https://gelbooru.com/index.php?page=account&s=home"
+          target="_blank"
+        >
+          My account
+        </a>
+      </div>
       <GelbooruGoToPostId :button-props="{ flat: true, color: 'white' }" />
     </div>
   </div>
@@ -54,6 +82,7 @@
 const router = useRouter()
 
 const { data: postCount, refresh, status } = useFetch('/api/post/count')
+useIntervalFn(refresh, 60_000)
 
 const searchTags = ref<string[]>([])
 function onSubmitSearch() {
@@ -70,6 +99,12 @@ function onSubmitSearch() {
 </script>
 
 <style scoped>
+
+.links-hero-wrapper {
+  a {
+    @apply tw-text-primary hover:tw-text-accent tw-transition-colors tw-no-underline tw-text-lg
+  }
+}
 
 .counter-hero-banner::before {
   content: "";

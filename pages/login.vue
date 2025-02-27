07e05a5079068b63ac4e4056a7ceefd9c35936d7
Emilio P <ea.pugaavalos.99@gmail.com>
2025-02-26 23:53:22 -0600

    Add paste functionality back into the login form

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tw-flex tw-h-[80dvh] tw-flex-col tw-items-center tw-justify-center">
     <QCard flat>
-      <QForm @submit="onSubmitLogin">
+      <QForm @submit="onSubmitLogin" @paste.prevent="onPasteLogin">
         <QCardSection class="tw-flex tw-flex-col tw-gap-2">
           <h1 class="text-h5">
             Login
@@ -37,10 +37,10 @@
             Your credentials won't get stored anywhere inside the server.
           </p>
           <p>
-            To access the Gelbooru API we need your access credentials. You can see your credentials in <a href="https://gelbooru.com/index.php?page=account&s=options">here</a>
+            To access the Gelbooru API it's necessary to have your API access credentials. You can see your credentials in <a href="https://gelbooru.com/index.php?page=account&s=options">here</a>
           </p>
           <p>
-            (You can paste your API credentials inside the textbox)
+            (You can paste your API credentials inside the textbox and it'll fill out the user ID and api key automatically)
           </p>
         </QCardSection>
         <QCardSection class="tw-flex tw-flex-col">
@@ -59,6 +59,7 @@
 
 <script setup lang="ts">
 
+const quasar = useQuasar()
 const router = useRouter()
 
 const authStore = useAuthStore()
@@ -70,7 +71,6 @@ const loginForm = ref({
 })
 
 async function onSubmitLogin() {
-
   loadingForm.value = true
 
   authStore.login(loginForm.value.api_key, loginForm.value.user_id)
@@ -82,6 +82,53 @@ async function onSubmitLogin() {
     console.log('Error', err)
     loadingForm.value = false
   })
+}
+
+// To check if it's a valid api key
+const testRegApiAccessCreds = /^&api_key=.*&user_id=\d*$/
+// To extract the api key
+const apiKeyReg = /(?<=^&api_key=)(.*)(?=&user_id)/
+// To extract the user id
+const userIdReg = /(?<=&user_id=)(.*)$/
+async function onPasteLogin(e: ClipboardEvent) {
+  try {
+    if (!e.clipboardData) {
+      quasar.notify({
+        message: 'No clipboard data',
+        color: 'negative',
+      })
+      return
+    }
+
+    const {clipboardData} = e
+    const text = clipboardData.getData('text')
+
+    const testValidApiAccessCreds = testRegApiAccessCreds.test(text)
+    if (!testValidApiAccessCreds) {
+      quasar.notify({
+        message: 'Invalid API access credentials',
+        color: 'negative',
+      })
+      return
+    }
+
+    const apiKey = apiKeyReg.exec(text)?.[0]
+    loginForm.value.api_key = apiKey ?? ''
+
+    const userId = userIdReg.exec(text)?.[0]
+    loginForm.value.user_id = userId ?? ''
+
+
+    e.preventDefault()
+
+  } catch (error) {
+    console.error(error)
+    quasar.notify({
+      caption: 'Error',
+      message: 'Unknown error while trying to parse the API access credentials, see console for more information',
+      color: 'negative',
+    })
+  }
 }
 
 </script>

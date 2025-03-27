661546671a1c7f5d5a60b9a1694a9a60649a7455
Emilio P <ea.pugaavalos.99@gmail.com>
2025-03-26 22:07:07 -0600

    Add cache to the post global counter

--- server/api/post/count.get.ts
+++ server/api/post/count.get.ts
@@ -1,17 +1,35 @@
 import type { GelbooruPostRes } from '~/types/gelbooru'
 
 import axios_gelbooru from '~/server/axiosGelbooru'
+import cacheClient from '~/server/db/cache'
+
+interface GelbooruTotal {
+  total: number
+}
 
 export default defineEventHandler(async () => {
-  const resGel = await axios_gelbooru.get<GelbooruPostRes>('', {
-    params: {
-      page: 'dapi',
-      q: 'index',
-      s: 'post',
-      json: 1,
-      limit: 1,
-    },
-  })
+  try {
 
-  return resGel.data['@attributes'].count
+    const cachedTotal = await cacheClient.getKeyJson<GelbooruTotal>('posts:total')
+    if (cachedTotal) {
+      return cachedTotal.data.total
+    }
+
+    const resGel = await axios_gelbooru.get<GelbooruPostRes>('', {
+      params: {
+        page: 'dapi',
+        q: 'index',
+        s: 'post',
+        json: 1,
+        limit: 1,
+      },
+    })
+
+    cacheClient.setKeyJson('posts:total', { total: resGel.data['@attributes'].count }, 120)
+
+    return resGel.data['@attributes'].count
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 })

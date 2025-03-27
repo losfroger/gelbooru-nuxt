6b3a35d6c36526d980866bdc01b6bcbd90923db7
Emilio P <ea.pugaavalos.99@gmail.com>
2025-03-26 22:07:35 -0600

    Check the cache in health

--- server/api/health.ts
+++ server/api/health.ts
@@ -1,3 +1,14 @@
+import cacheClient from '../db/cache'
+
 export default defineEventHandler(async () => {
+  if (import.meta.env.NUXT_CACHE_ENABLED == 'true' && !cacheClient.isLoaded) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Error no cache',
+    })
+  }
+
+  cacheClient.setKeyJson('health', { healthy: true }, 0)
+
   return 'OK'
 })

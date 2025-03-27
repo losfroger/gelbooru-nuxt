60a1b86a20301b2bb129e5583efbc7cc6d535775
Emilio P <ea.pugaavalos.99@gmail.com>
2025-03-26 22:01:48 -0600

    Fix check session

--- server/api/auth/check-session.get.ts
+++ server/api/auth/check-session.get.ts
@@ -2,5 +2,5 @@ export default defineEventHandler(async (event) => {
   const api_key = getCookie(event, 'api_key')
   const user_id = getCookie(event, 'user_id')
 
-  return api_key && user_id
+  return (!!api_key && !!user_id)
 })
